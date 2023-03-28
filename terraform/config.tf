847007005d7b02e3cff4e49a9038048654b4864e
cygnus2601 <117513553+cygnus2601@users.noreply.github.com>
2023-03-28 09:11:04 +0530

    Update config.tf

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAUNRCPZLLMDMOVP5F"
-  secret_key = "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"
+  access_key = "XXXXXXXXXXXXXXX"
+  secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
 }
